2d2d329926635f43c7f29a0df195fd1f26b9187c
Ivan HG <iivanhg@outlook.com>
2026-06-26 21:27:40 -0600

    refactor: proyecto

--- ejemplo-04/src/main/java/mx/ipn/upiicsa/web/ejemplo03/contacts/persistence/repository/ContactJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package mx.ipn.upiicsa.web.ejemplo03.contacts.persistence.repository;
-
-import mx.ipn.upiicsa.web.ejemplo03.contacts.persistence.entity.ContactJpa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactJpaRepository extends JpaRepository<ContactJpa, Integer> {
-}
